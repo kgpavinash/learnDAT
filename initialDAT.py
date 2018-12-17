3f46aa78f66566b2c6595eb292a5c27fa70ca1d6
COMPANYNET\aprabhakar <kgpavinash@gmail.com>
2018-12-17 15:59:28 -0500

    comparing rows seem to be working after moving

--- initialDAT.py
+++ initialDAT.py
@@ -2,12 +2,12 @@ import sqlite3
 import json
 
 #Read data from json (Socrata Database)
-f = open("simplejson3.txt", "r")
+f = open("simplejson.txt", "r")
 content = f.read()
 dict_all = json.loads(content)
 
 #Connection to SQLite Database
-conn = sqlite3.connect('simple.db',isolation_level=None)
+conn = sqlite3.connect('testing0.db',isolation_level=None)
 c = conn.cursor()
 
 #Get count of all tables in the database
@@ -69,6 +69,7 @@ if (len(tables) == 0):
     conn.close()
     exit()
 
+
 #Print all entries in a table
 # c.execute('''SELECT * FROM table1''')
 # for row in c:
@@ -97,8 +98,69 @@ for i in range(len(latestColumns)):
                 c.executemany(insertQuery, entries)
                 conn.commit()
                 conn.close()
+                exit()
 
-#TBD compare values!
+
+#TBD compare values! I have to create a new table in order to do value comparison
+
+print("---------------------------------")
+
+print(latestTable)
+print(tableName)
+
+newTable = tableName
+c.execute(createQuery)
+c.executemany(insertQuery, entries)
+
+rowsRemovedCount = []
+leftJoinStatement = "SELECT COUNT(*) FROM "+latestTable+" LEFT OUTER JOIN "+newTable+" ON "+latestTable+".ndc = "+newTable+".ndc WHERE "+newTable+".year ISNULL"
+c.execute(leftJoinStatement)
+for row in c:
+    print(row)
+    rowsRemovedCount.append(row)
+rowsAddedCount = []
+revLeftJoinStatement = "SELECT COUNT(*) FROM "+newTable+" LEFT OUTER JOIN "+latestTable+" ON "+latestTable+".ndc = "+newTable+".ndc WHERE "+latestTable+".year ISNULL"
+c.execute(revLeftJoinStatement)
+for row in c:
+    print(row)
+    rowsAddedCount.append(row)
+
+latestTableRowsCount = []
+c.execute("SELECT COUNT(*) FROM "+latestTable)
+for row in c:
+    #print(row)
+    latestTableRowsCount.append(row)
+
+newTableRowsCount = []
+c.execute("SELECT COUNT(*) FROM "+newTable)
+for row in c:
+    #print(row)
+    newTableRowsCount.append(row)
+
+checkEmpty = 0
+print(str(rowsRemovedCount[0][0])+ " rows has been removed from the old table which had "+str(latestTableRowsCount[0][0]) + " rows")
+print(str(rowsAddedCount[0][0])+ " rows has been added to the new table which now has "+str(newTableRowsCount[0][0]) + " rows")
+if int(newTableRowsCount[0][0]) == 0:
+    print("Shrinkage of 100%")
+    print("Growth of 0%")
+    checkEmpty = 1
+if int(latestTableRowsCount[0][0]) == 0:
+    print("Shrinkage of 0%")
+    print("Growth of 100%")
+    checkEmpty = 1
+
+if checkEmpty == 0:
+    shrinkage = str(int(rowsRemovedCount[0][0]) / int(latestTableRowsCount[0][0]) * 100)
+    print("Shrinkage of "+shrinkage+"%")
+    
+    growth = str(int(rowsAddedCount[0][0]) / int(newTableRowsCount[0][0]) * 100)
+    print("Growth of "+growth+"%")
+
+c.execute("DROP TABLE " + newTable)
+
+
+
+
 
 #Create a new table and populate it. (If there are any differences)
 # if hasColumnChanged is False:
