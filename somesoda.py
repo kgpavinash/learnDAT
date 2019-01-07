65d4f8e8c2eee6e10fd5c1247af3881ba5e642f1
Prabhakar <aprabhakar@companynet.org>
2019-01-07 15:22:52 -0500

    more readable code in somesoda.py

--- somesoda.py
+++ somesoda.py
@@ -34,8 +34,8 @@ jsonFormatMetadata = json.dumps(metadata, indent=4)
 #print(jsonFormatMetadata)
 
 #write first 50,000 results to file
-f = open("jsonResult0.txt", "w+")
-f.write(jsonFormat)
+# f = open("jsonResult0.txt", "w+")
+# f.write(jsonFormat)
 
 #get actual count of results
 result = client.get(medic_identifier, query=finalQuery2)
@@ -43,18 +43,35 @@ print(result)
 print("-----")
 
 #Keep getting results until it is empty. Write every 50,000 results to file. Keep track of number of files
-countFile = 0
-countOffset = 50000
-result = client.get(medic_identifier, query=finalQuery3)
-while result:
-    countFile = countFile + 1
-    countOffset = countOffset + 50000
-    jsonFormat = json.dumps(result, indent=4)
-    f = open("jsonResult"+str(countFile)+".txt", "w+")
-    f.write(jsonFormat)
-    finalQuery3 = 'SELECT * WHERE year = ' + maxyear +' AND quarter = '+maxQuarter+' ORDER BY ndc DESC OFFSET '+str(countOffset)+' LIMIT 50000'
-    result = client.get(medic_identifier, query=finalQuery3)
+# countFile = 0
+# countOffset = 50000
+# result = client.get(medic_identifier, query=finalQuery3)
+# while result:
+#     countFile = countFile + 1
+#     countOffset = countOffset + 50000
+#     jsonFormat = json.dumps(result, indent=4)
+#     f = open("jsonResult"+str(countFile)+".txt", "w+")
+#     f.write(jsonFormat)
+#     finalQuery3 = 'SELECT * WHERE year = ' + maxyear +' AND quarter = '+maxQuarter+' ORDER BY ndc DESC OFFSET '+str(countOffset)+' LIMIT 50000'
+#     result = client.get(medic_identifier, query=finalQuery3)
 
-#write count of files to file.
+# #write count of files to file.
+# f = open("countFile.txt", "w+")
+# f.write(str(countFile))
+
+countFiles = 0
+countOffset = 0
+while 1:
+    loopQuery = 'SELECT * WHERE year = ' + maxyear +' AND quarter = '+maxQuarter+' ORDER BY ndc DESC OFFSET '+str(countOffset)+' LIMIT 50000'
+    result = client.get(medic_identifier, query=loopQuery)
+    if not result:
+        break
+    jsonFormat = json.dumps(result, indent=4)
+    f = open("jsonResult"+str(countFiles)+".txt", "w+")
+    f.write(jsonFormat)
+    countOffset = countOffset + 50000
+    countFiles = countFiles + 1
+
+countFiles = countFiles - 1
 f = open("countFile.txt", "w+")
-f.write(str(countFile))
+f.write(str(countFiles))
